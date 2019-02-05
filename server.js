58734acdc39ee63604835d395aa5ad3b0f0e9252
fkakatie <sextonskatie@gmail.com>
2019-02-04 18:13:26 -0700

    setup express and middleware

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 // required packages
 var express = require('express');
 var path = require('path');
+
+// setup express
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
