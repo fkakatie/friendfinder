a7cd141954ea39988902c9997f67a73cb2a05dc5
fkakatie <sextonskatie@gmail.com>
2019-02-07 20:20:56 -0700

    setup friends array

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+// store all friends in an array
+
+var friends = [
+    {
+        username: "Katie",
+        profPic: "https://bit.ly/2MTTLvO",
+        survey: [1, 4, 3, 4, 3, 4, 5, 4, 1, 5],
+        mbtiType: "ISFJ-T"
+    }
+];
+
+module.exports = friends;
