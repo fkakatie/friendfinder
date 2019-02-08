0cbbe7feaac3280bcbe0372affa481c252c2596c
fkakatie <sextonskatie@gmail.com>
2019-02-07 19:55:42 -0700

    started server

--- server.js
+++ server.js
@@ -9,3 +9,12 @@ var PORT = process.env.PORT || 3000;
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// setup routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// start the server
+app.listen(PORT, function() {
+    console.log("friend finder ready on http://localhost:" + PORT);
+});
