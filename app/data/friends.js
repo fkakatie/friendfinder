2848420a6190ffbac6e58ffdaed469e352c90842
fkakatie <sextonskatie@gmail.com>
2019-02-09 12:51:38 -0700

    npm init, packages installed

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,6 +5,7 @@ var friends = [
         username: "Katie",
         profPic: "https://bit.ly/2MTTLvO",
         survey: [1, 4, 3, 4, 3, 4, 5, 4, 1, 5],
+        mbtiArray: [3, 5, 5, 7, 2],
         mbtiType: "ISFJ-T"
     }
 ];
