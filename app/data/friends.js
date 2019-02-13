b998f7187c7209b0e38a916a7ffa0b3cfb68693b
fkakatie <sextonskatie@gmail.com>
2019-02-12 19:47:46 -0700

    new friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,12 +2,293 @@
 
 var friends = [
     {
-        username: "Katie",
-        profPic: "https://bit.ly/2MTTLvO",
-        survey: [1, 4, 3, 4, 3, 4, 5, 4, 1, 5],
-        mbtiArray: [3, 5, 5, 7, 2],
-        mbtiType: "ISFJ-T"
+        username: "Abigail",
+        profPic: "assets/images/abigail.png",
+        survey: [
+            "1",
+            "4",
+            "5",
+            "2",
+            "3",
+            "1",
+            "4",
+            "3",
+            "4",
+            "2"
+        ],
+        mbtiArray: [
+            "3",
+            "9",
+            "8",
+            "7",
+            "8"
+        ],
+        "mbtiType": "INTJ-A"
+    },
+    {
+        username: "Alex",
+        profPic: "assets/images/alex.png",
+        survey: [
+            "5",
+            "2",
+            "2",
+            "3",
+            "2",
+            "1",
+            "1",
+            "3",
+            "5",
+            "4"
+        ],
+        mbtiArray: [
+            "9",
+            "5",
+            "7",
+            "4",
+            "7"
+        ],
+        "mbtiType": "ESTP-A"
+    },
+    {
+        username: "Elliot",
+        profPic: "assets/images/elliott.png",
+        survey: [
+            "5",
+            "3",
+            "5",
+            "1",
+            "3",
+            "4",
+            "4",
+            "4",
+            "4",
+            "2"
+        ],
+        mbtiArray: [
+            "8",
+            "10",
+            "5",
+            "7",
+            "8"
+        ],
+        "mbtiType": "ENTP-A"
+    },
+    {
+        username: "Emily",
+        profPic: "assets/images/emily.png",
+        survey: [
+            "5",
+            "2",
+            "5",
+            "1",
+            "1",
+            "4",
+            "2",
+            "5",
+            "5",
+            "2"
+        ],
+        mbtiArray: [
+            "9",
+            "10",
+            "3",
+            "3",
+            "9"
+        ],
+        "mbtiType": "ENFP-A"
+    },
+    {
+        username: "Haley",
+        profPic: "assets/images/haley.png",
+        survey: [
+            "4",
+            "1",
+            "4",
+            "2",
+            "2",
+            "2",
+            "3",
+            "4",
+            "3",
+            "4"
+        ],
+        mbtiArray: [
+            "9",
+            "8",
+            "7",
+            "7",
+            "5"
+        ],
+        "mbtiType": "ENTJ-T"
+    },
+    {
+        username: "Harvey",
+        profPic: "assets/images/harvey.png",
+        survey: [
+            "2",
+            "4",
+            "3",
+            "3",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "4"
+        ],
+        mbtiArray: [
+            "4",
+            "7",
+            "10",
+            "6",
+            "4"
+        ],
+        "mbtiType": "INTP-T"
+    },
+    {
+        username: "Leah",
+        profPic: "assets/images/leah.png",
+        survey: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "2",
+            "4",
+            "2",
+            "4",
+            "5",
+            "3"
+        ],
+        mbtiArray: [
+            "6",
+            "6",
+            "4",
+            "4",
+            "8"
+        ],
+        "mbtiType": "ISFP-A"
+    },
+    {
+        username: "Maru",
+        profPic: "assets/images/maru.png",
+        survey: [
+            "2",
+            "2",
+            "4",
+            "4",
+            "5",
+            "2",
+            "4",
+            "4",
+            "3",
+            "4"
+        ],
+        mbtiArray: [
+            "6",
+            "6",
+            "9",
+            "7",
+            "5"
+        ],
+        "mbtiType": "ISTP-T"
+    },
+    {
+        username: "Penny",
+        profPic: "assets/images/penny.png",
+        survey: [
+            "1",
+            "5",
+            "3",
+            "2",
+            "2",
+            "3",
+            "4",
+            "2",
+            "5",
+            "4"
+        ],
+        mbtiArray: [
+            "2",
+            "7",
+            "5",
+            "8",
+            "7"
+        ],
+        "mbtiType": "INFJ-A"
+    },
+    {
+        username: "Sam",
+        profPic: "assets/images/sam.png",
+        survey: [
+            "4",
+            "1",
+            "4",
+            "4",
+            "1",
+            "3",
+            "1",
+            "1",
+            "5",
+            "2"
+        ],
+        mbtiArray: [
+            "9",
+            "6",
+            "4",
+            "6",
+            "9"
+        ],
+        "mbtiType": "ESFP-A"
+    },
+    {
+        username: "Sebastian",
+        profPic: "assets/images/sebastian.png",
+        survey: [
+            "2",
+            "4",
+            "4",
+            "5",
+            "4",
+            "1",
+            "4",
+            "1",
+            "5",
+            "3"
+        ],
+        mbtiArray: [
+            "4",
+            "5",
+            "9",
+            "9",
+            "8"
+        ],
+        "mbtiType": "ISTJ-A"
+    },
+    {
+        username: "Shane",
+        profPic: "assets/images/shane.png",
+        survey: [
+            "1",
+            "2",
+            "3",
+            "3",
+            "3",
+            "3",
+            "2",
+            "4",
+            "1",
+            "5"
+        ],
+        mbtiArray: [
+            "5",
+            "7",
+            "6",
+            "4",
+            "2"
+        ],
+        "mbtiType": "INFP-T"
     }
-];
+]
 
 module.exports = friends;
